7fb5e645ee22dcaf8e192a8dfbe12fed1ec495f5
Kunal Gangani <thekunalgangani@gmail.com>
2024-01-20 23:11:01 +0530

    Added

--- 1_2.cpp
+++ 1_2.cpp
@@ -17,8 +17,6 @@ using namespace std;
 class P{
 	public:
 		double celsius;
-		double fahrenheit;
-		double kelvin;
 		void takeInput(){
 			cout<<"Enter Celsius : ";
 			cin>>celsius;
@@ -27,15 +25,13 @@ class P{
 class Q : public P{
 	public:
 		void toFahrenheit(){
-			fahrenheit=(18×celsius)+32;
-			cout<<celsius<<" Celsius is "<<fahrenheit<<" Fahrenheit "<<endl;
+			cout<<" Celsius is "<<1.8×celsius+32<<" Fahrenheit "<<endl;
 		}
 };
 class R : public Q{
 	public:
 		void toKelvin(){
-			kelvin=(32*fahrenheit-32)×5/9+273.15;
-			cout<<"Fahrenheit "<<fahrenheit<<" to "<<kelvin<<" Kelvin ";
+			cout<<"Celsius is "<<celsius<<" Kelvin is "<<celsius+273.15;
 		}
 };
 int main(){
